31e5b89db01c5c98cdfd4bb07c25b8b8d6bb1c24
KryptNit <meoxwklktr@gmail.com>
2025-04-19 19:10:16 +0700

    initial commit

---
+++ keymaps/default/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define I2C_DRIVER I2CD0
+#define I2C1_SDA_PIN GP0
+#define I2C1_SCL_PIN GP1
+#define OLED_DISPLAY_128X64
---
+++ keymaps/vial/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x1D, 0xDA, 0x23, 0x61, 0x38, 0x4E, 0x60, 0xC7}
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+#define I2C_DRIVER I2CD0
+#define I2C1_SDA_PIN GP0
+#define I2C1_SCL_PIN GP1
+#define OLED_DISPLAY_128X64
